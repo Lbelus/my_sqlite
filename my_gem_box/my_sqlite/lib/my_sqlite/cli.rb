1bcb07aa3722f79c56ae23cd5089f0d8308efee6
LBelus <lorris.belus@icloud.com>
2023-07-31 10:59:42 +0000

    add more tests

--- my_gem_box/my_sqlite/lib/my_sqlite/cli.rb
+++ my_gem_box/my_sqlite/lib/my_sqlite/cli.rb
@@ -50,7 +50,7 @@ module QueryMethods
         while (1) 
             text += __get_input__
             if text[text.length() - 1] == ';'
-                text = text.chop
+                text = text.chop #removes the last char
                 break;
             end
             text += ' '
@@ -66,6 +66,10 @@ module QueryMethods
         elsif (query[0].casecmp("select") == 0 && valid_select?(query))
             return false
         elsif (query[0].casecmp("update") == 0 && valid_update?(query))
+            return false
+        elsif (query[0].casecmp("delete") == 0 && valid_delete?(query))
+            return false
+        elsif (query[0].casecmp("insert") == 0 && valid_insert?(query))
             return false
         else 
             return true;
@@ -110,8 +114,40 @@ module QueryMethods
             return false
         elsif !valid_set?(query)
             return false
-        elsif (!valid_update_where?(query, find_keyword_idx(query, 'where')))
+        elsif (!valid_update_delete_where?(query, find_keyword_idx(query, 'where')))
             return false
+        end
+        return true
+    end
+    
+    ##################### valid_delete? ################
+    def valid_delete?(query)
+        if query.empty?()
+            return false
+        elsif query[0].casecmp("delete") != 0
+            return false
+        elsif query[1].casecmp("from") != 0
+            return false
+        elsif (!valid_update_delete_where?(query, find_keyword_idx(query, 'where')))
+            return false
+        end
+        return true
+    end
+    
+    ##################### valid_insert? ################
+    def valid_insert?(query)
+        if query.empty?()
+            return false
+        elsif query.length < 5
+        elsif query[0].casecmp("insert") != 0
+            return false
+        elsif query[1].casecmp("into") != 0
+            return false
+        elsif query[3].casecmp("values") != 0 &&
+              query[4].casecmp("values") != 0  
+            return false
+        # TODO 
+        # check that the number of columns matche the number of values
         end
         return true
     end
@@ -182,8 +218,8 @@ module QueryMethods
     end
 
     
-    ##################### valid_update_where? ##################
-    def valid_update_where?(query, idx)
+    ##################### valid_update_delete_where? ##################
+    def valid_update_delete_where?(query, idx)
         #there is no where keyword
         if idx == nil
             return true
@@ -282,9 +318,33 @@ module QueryMethods
                 start += 3
             end
             q.set = data
+        elsif query[0].casecmp("delete") == 0
+            q.delete = true
+            q.from = query[2]
+        elsif query[0].casecmp("insert") == 0
+            q.insert = query[2]
+            data = {}
+            vals = Array[]
+            keys = Array[]
+            has_values = 0
+            if (query[3].casecmp("values") != 0) 
+                keys = query[3].split(',')
+                has_values = 1
+            end
+                 
+            vals = query[query.length - 1].split(',')
+            vals.length.times do |i|
+                if (has_values == 0)
+                    key = "generic_header_" + (i + 1).to_s
+                    data[key] = vals[i]
+                else
+                    data[keys[i]] = vals[i]
+                end
+            end
+            q.values = data
         end
-        get_where_cndt(query, q)        
-        q
+        get_where_cndt(query, q) 
+        q       
     end
 
     ##################### find_where_idx ##################
@@ -313,14 +373,15 @@ module QueryMethods
         if query.empty?()
             return nil
         end
-        keyword = query[0..5]
-        if keyword.casecmp("select") == 0
-            query.gsub!(", ", ",")
-            query.gsub!(" ,", ",")
-
-        elsif keyword.casecmp("update") == 0    
-            query.gsub!(", ", " ")
-            query.gsub!(" ,", " ")
+        keyword = query[0..5] #extract first six chars
+        if  (keyword.casecmp("select") == 0 ||
+            keyword.casecmp("update") == 0 ||
+            keyword.casecmp("delete") == 0 ||
+            keyword.casecmp("insert") == 0)
+                query.gsub!(", ", ",")
+                query.gsub!(" ,", ",")
+                query.gsub!("(", " ")
+                query.gsub!(")", " ")
         else
             return nil
         end
--- my_gem_box/my_sqlite/test/test_my_sqlite.rb
+++ my_gem_box/my_sqlite/test/test_my_sqlite.rb
@@ -37,6 +37,38 @@ class MySqliteInstanceTest < Minitest::Test
     # assert_equal 'stubbed value', result
     end
 
+    def test_cli_delete
+        test_array = [
+            ["delete","from data.csv","where job = Engineer",";"]
+        ]
+        test_array.each do |_test_|
+            puts "/!\\ Testing DELETE against #{_test_}:\n\n"
+            MySqliteInstance.any_instance.stubs(:__get_input__).returns(*_test_)
+            sqlite_instance = MySqliteInstance.new
+            result = sqlite_instance.instanciation
+            puts "\n\n"
+        end
+    # assert_equal 'stubbed value', result
+    end
+
+    
+
+
+    def test_cli_insert
+        test_array = [
+            ["insert into data.csv","VALUES 16, 'Spooder', 'Man', 'ceiling_crawler', 23",";"],
+            ["insert into data.csv","VALUES 16, 'Spooder', 'Man', 'ceiling crawler', 23",";"]
+        ]
+        test_array.each do |_test_|
+            puts "/!\\ Testing INSERT against #{_test_}:\n\n"
+            MySqliteInstance.any_instance.stubs(:__get_input__).returns(*_test_)
+            sqlite_instance = MySqliteInstance.new
+            result = sqlite_instance.instanciation
+            puts "\n\n"
+        end
+    # assert_equal 'stubbed value', result
+    end
+
     def test_cli_error
         test_array = [
             ["select error_col","from data.csv",";"],
@@ -60,5 +92,52 @@ class MySqliteInstanceTest < Minitest::Test
         # assert_equal 'stubbed value', result
     end
 
+    def test_methods
+        
+        request = MySqliteRequest.new
+        puts "/!\\ Testing METHODS FROM_WHERE_DELETE against:\n\n"
+        request.from('data.csv').where('job', 'Engineer').delete.run
+        puts
+        puts "/!\\ Testing METHODS FROM_JOIN against join first_name job:\n\n"
+        request = request.from('data.csv').join('first_name', 'data.csv', 'job').run
+        puts
+        puts "/!\\ Testing METHODS FROM ORDER against order job:\n\n"
+        request = request.from('data.csv').order(:asc,'job').run
+        puts
+        puts "/!\\ Testing METHODS SELECT WHERE #01:\n\n"
+        request = request.select('first_name').from('data.csv').where('job', 'Engineer').run
+        puts
+        puts "/!\\ Testing METHODS SELECT WHERE #02:\n\n"
+        request = request.select('*').from('data.csv').where('job', 'Engineer').run
+        puts
+
+        insert_data = {
+           'index' => 17,
+           'first_name' => 'Peter',
+           'last_name' => 'Parker',
+           'job' => 'Photographer',
+           'age' => 23
+        }
+
+        update_data = {
+           'index' => 15,
+           'first_name' => 'Spooder',
+           'last_name' => 'Man',
+           'job' => 'ceiling crawler'
+        }
+
+        set_data = {
+            'job' => "пенсионер",
+        }
+        puts "/!\\ Testing METHODS INSERT :\n\n"
+        request = request.insert('data.csv').values(insert_data).run
+        puts
+        puts "/!\\ Testing METHODS UPDATE VALUE :\n\n"
+        request = request.update('data.csv').values(update_data).run
+        puts
+        puts "/!\\ Testing METHODS SET VALUE :\n\n"
+        request = request.update('data.csv').set(set_data).where('job', 'Engineer').run
+        puts
+    end
 
 end
